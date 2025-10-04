517d8ce01efe96f6c5f7a766f1193ea865f38c4d
Simon Hans Hille <simon.hille@physik.uzh.ch>
2025-10-04 13:46:15 +0200

    why not

--- src/apublishtest/_version.py
+++ src/apublishtest/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '1.0.0.post1.dev4'
-__version_tuple__ = version_tuple = (1, 0, 0, 'post1', 'dev4')
+__version__ = version = '1.0.0.post1.dev5'
+__version_tuple__ = version_tuple = (1, 0, 0, 'post1', 'dev5')
 
 __commit_id__ = commit_id = None
